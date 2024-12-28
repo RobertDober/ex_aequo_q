ae98043f1da5eb9434bab4f10ab03d6c28a575a6
RobertDober <robert.dober@gmail.com>
2024-12-28 12:00:30 +0100

    mix new on local

---
+++ lib/ex_aequo_q.ex
@@ -0,0 +1,18 @@
+defmodule ExAequoQ do
+  @moduledoc """
+  Documentation for `ExAequoQ`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAequoQ.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_aequo_q_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAequoQTest do
+  use ExUnit.Case
+  doctest ExAequoQ
+
+  test "greets the world" do
+    assert ExAequoQ.hello() == :world
+  end
+end
